9d3acd0c953ed143d726356c2a48f28aa68efbdf
Sanjay K <sanjay.work@hotmail.com>
2021-04-25 20:30:42 +0530

    Updated solution for bob

--- bob/bob.go
+++ bob/bob.go
@@ -1,31 +1,29 @@
 package bob
 
 import (
-	"regexp"
 	"strings"
 )
 
 func Hey(remark string) string {
 
 	remark = strings.TrimSpace(remark)
-
 	if remark == "" {
 
 		return "Fine. Be that way!"
 
 	}
 
-	if isYell(remark) && remark[len(remark)-1:] != "?" {
-
-		return "Whoa, chill out!"
-	}
-
-	if isYellQuestion(remark) {
+	if isYell(remark) && Question(remark) {
 
 		return "Calm down, I know what I'm doing!"
 	}
 
-	if remark[len(remark)-1:] == "?" {
+	if isYell(remark) {
+
+		return "Whoa, chill out!"
+	}
+
+	if Question(remark) {
 
 		return "Sure."
 	}
@@ -33,23 +31,13 @@ func Hey(remark string) string {
 	return "Whatever."
 }
 
-var charRegex = regexp.MustCompile(`[a-zA-Z]+`)
-
 func isYell(remark string) bool {
 
-	hasCharacters := charRegex.MatchString(remark)
-	allUpper := strings.ToUpper(remark) == remark
-
-	return hasCharacters && allUpper
+	return remark == strings.ToUpper(remark) && remark != strings.ToLower(remark)
 }
 
-func isYellQuestion(remark string) bool {
+func Question(remark string) bool {
 
-	hasCharacters := charRegex.MatchString(remark)
+	return remark[len(remark)-1:] == "?"
 
-	allUpper := strings.ToUpper(remark) == remark
-
-	questionMark := remark[len(remark)-1:] == "?"
-
-	return hasCharacters && allUpper && questionMark
 }
