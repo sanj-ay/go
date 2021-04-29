f084e5a36c8ed53cb586c4531aa5170df42c7378
Sanjay K <sanjay.work@hotmail.com>
2021-04-29 13:08:39 +0530

    Completed accumulate exercise

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,1 +1,9 @@
 package accumulate
+
+func Accumulate(collection []string,transformation func(string) string)[]string{
+	var result []string
+	for i :=0;i<len(collection);i++{
+		result = append(result,transformation(collection[i]))
+	}
+	return result
+}
