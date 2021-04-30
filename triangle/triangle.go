379f2fff6068aab9ca0bd60999bb3d6b001df07a
Sanjay K <sanjay.work@hotmail.com>
2021-04-30 14:13:39 +0530

    Completed triangle exercise

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,28 +1,36 @@
 // This is a "stub" file.  It's a little start on your solution.
 // It's not a complete solution though; you have to write some code.
 
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+import "math"
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	NaT = 0
+	Equ = 1
+	Iso = 2
+	Sca = 3
 )
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	var k Kind
+	pinf := math.Inf(1)
+	ninf := math.Inf(-1)
+	if !(a > 0 && b > 0 && c > 0 &&
+		a != pinf && b != pinf && c != pinf && a != ninf && b != ninf && c != ninf && a+b >= c &&
+		b+c >= a && a+c >= b) {
+		k = NaT
+	} else if a == b && b == c {
+		k = Equ
+	} else if a == b || a == c || b == c {
+		k = Iso
+	} else {
+		k = Sca
+	}
+
 	return k
+
 }
