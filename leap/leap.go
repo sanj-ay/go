663621f50366706e3e4720a194955844afd4e9c6
Sanjay K <sanjay.work@hotmail.com>
2021-04-30 17:00:51 +0530

    completed rna-transcription exercise

--- leap/leap.go
+++ leap/leap.go
@@ -1,9 +1,8 @@
-
 package leap
 
 func IsLeapYear(year int) bool {
-	if year % 4 == 0 {
-		if year % 100 == 0 && year %400 != 0{
+	if year%4 == 0 {
+		if year%100 == 0 && year%400 != 0 {
 			return false
 		}
 		return true
--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,14 @@
 package strand
 
 func ToRNA(dna string) string {
+	complement := map[rune]rune{
+		'G':'C','C':'G','T':'A','A':'U',
+	}
+	runes :=[]rune(dna)
+	for i :=0;i<len(runes);i++{
+		runes[i] = complement[runes[i]]
+	}
+
+	return string(runes)
 }
+
