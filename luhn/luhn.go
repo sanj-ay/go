a9785626db902644343deb8dd00908f0392a0747
Sanjay K <sanjay.work@hotmail.com>
2021-05-01 08:22:40 +0530

    Updated luhn algorithm

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -5,34 +5,31 @@ import (
 	"unicode"
 )
 
-func Valid(number string)bool{
+func Valid(number string) bool {
 	number = strings.ReplaceAll(number, " ", "")
 	runes := []rune(number)
 	sum := 0
 	alternative := false
-	if len(runes) <2{
+	if len(runes) < 2 {
 		return false
 	}
 	for i := len(runes) - 1; i >= 0; i-- {
-		if !unicode.IsDigit(runes[i]){
+		if !unicode.IsDigit(runes[i]) {
 			return false
 		}
 		if alternative {
 			d := runes[i] - '0'
-			if d *=2; d> 9 {
+			if d *= 2; d > 9 {
 				sum += int(d) - 9
 			} else {
 				sum += int(d)
 			}
-			alternative = !alternative
+
 		} else {
 			d := runes[i] - '0'
 			sum = sum + int(d)
-			alternative = !alternative
 		}
+		alternative = !alternative
 	}
-	if sum%10 == 0 {
-		return true
-	}
-	return false
+	return sum%10 == 0
 }
--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -2,13 +2,12 @@ package strand
 
 func ToRNA(dna string) string {
 	complement := map[rune]rune{
-		'G':'C','C':'G','T':'A','A':'U',
+		'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U',
 	}
-	runes :=[]rune(dna)
-	for i :=0;i<len(runes);i++{
+	runes := []rune(dna)
+	for i := 0; i < len(runes); i++ {
 		runes[i] = complement[runes[i]]
 	}
 
 	return string(runes)
 }
-
