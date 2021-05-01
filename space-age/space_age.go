f2e8ad212cb81c81f86683ddf590eea6ea91bf60
Sanjay K <sanjay.work@hotmail.com>
2021-05-01 17:25:47 +0530

    Modified space age solution

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -3,24 +3,22 @@ package space
 type Planet string
 
 func Age(seconds float64, planet Planet) float64 {
-	var age float64
+	var age = seconds / 31557600
 	switch planet {
 	case "Mercury":
-		age = (seconds / 31557600) / 0.2408467
+		age = age / 0.2408467
 	case "Venus":
-		age = (seconds / 31557600) / 0.61519726
+		age = age / 0.61519726
 	case "Mars":
-		age = (seconds / 31557600) / 1.8808158
+		age = age / 1.8808158
 	case "Jupiter":
-		age = (seconds / 31557600) / 11.862615
+		age = age / 11.862615
 	case "Saturn":
-		age = (seconds / 31557600) / 29.447498
+		age = age / 29.447498
 	case "Uranus":
-		age = (seconds / 31557600) / 84.016846
+		age = age / 84.016846
 	case "Neptune":
-		age = (seconds / 31557600) / 164.79132
-	default:
-		age = seconds / 31557600
+		age = age / 164.79132
 	}
 	return age
 
