c41be4661ed7289bf42b53ccfdb747f24841fd29
Sanjay K <sanjay.work@hotmail.com>
2021-05-04 20:15:14 +0530

    Updated solution for grains

--- grains/grains.go
+++ grains/grains.go
@@ -3,16 +3,10 @@ package grains
 import "errors"
 
 func Total() uint64 {
-	var total uint64
-	for i := 1; i < 65; i++ {
-		n, _ := Square(i)
-		total += n
-	}
-	return total
-
+	return 0x1p64 -1
 }
 func Square(n int) (uint64, error) {
-	if n < 1 || n > 64 {
+	if n <= 0 || n > 64 {
 		return 0, errors.New("Invalid Number Of Squares")
 	}
 	return 1 << (n - 1), nil
