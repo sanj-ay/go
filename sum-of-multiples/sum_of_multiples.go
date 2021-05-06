857e5ac881eafeb77063cf403aa9fc92a9c8c934
Sanjay K <sanjay.work@hotmail.com>
2021-05-07 00:44:06 +0530

    Added solution for sum of multiples

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,17 @@
 package summultiples
+
+func SumMultiples(limit int, divisors []int) int {
+	sum := 0
+	for i := 1; i < limit; i++ {
+		for j := 0; j < len(divisors); j++ {
+			if divisors[j] == 0 {
+				continue
+			}
+			if i%divisors[j] == 0 {
+				sum = sum + i
+				break
+			}
+		}
+	}
+	return sum
+}
