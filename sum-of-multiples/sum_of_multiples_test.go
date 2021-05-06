acc961aa623ef9229fc6ccfcf6c0a7423bba1c60
Sanjay K <sanjay.work@hotmail.com>
2021-05-07 00:48:24 +0530

    Updated solution for sum of multiples

--- sum-of-multiples/sum_of_multiples_test.go
+++ sum-of-multiples/sum_of_multiples_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSumMultiples(t *testing.T) {
 	for _, test := range varTests {
-		s := SumMultiples(test.limit, test.divisors)
+		s := SumMultiples(test.limit, test.divisors...)
 		if s != test.sum {
 			t.Fatalf("Sum of multiples of %v to %d returned %d, want %d.",
 				test.divisors, test.limit, s, test.sum)
@@ -15,7 +15,7 @@ func TestSumMultiples(t *testing.T) {
 func BenchmarkSumMultiples(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, test := range varTests {
-			SumMultiples(test.limit, test.divisors)
+			SumMultiples(test.limit, test.divisors...)
 		}
 	}
 }
